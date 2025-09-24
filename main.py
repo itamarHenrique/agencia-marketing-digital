a69c84104428ee8da7586e3b3627561fd17977e7
itamarHenrique <itamarhjp1@gmail.com>
2025-09-23 23:32:46 -0300

    feat: inserção da funcionalidade de pegar_metricas e da função de usuario atual

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
 import pandas as pd
 from pydantic import BaseModel
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 
 
 app = FastAPI()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 SECRET_KEY = "teste"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -24,6 +27,16 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        role = payload.get("role")
+        if username is None or role is None:
+            raise HTTPException(status_code=401, detail="Credenciais invalidas")
+        return {"username": username, "role": role}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token invalido")
 
 class UserLogin(BaseModel):
     username: str
@@ -89,4 +102,16 @@ def cadastro(user: UserRegister):
     
     return {
         "message": "Usuário cadastrado com sucesso"
+    }
+    
+    
+@app.get("/metrics")
+def pegar_metricas(current_user: dict = Depends(get_current_user)):
+    if current_user['role'] != 'admin':
+        raise HTTPException(status_code=403, detail="Acesso negado")
+    
+    return {
+        "message": f"Bem-vindo {current_user['username']}!",
+        "role": current_user['role'],
+        "metrics": df_metrics.to_dict(orient="records")
     }
